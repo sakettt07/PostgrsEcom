ee6b9734ebe7fc0063dd9faa63f8b6c21db42e65
saket <saket@saketgupta.com>
2025-10-13 23:53:17 +0530

    feat(auth): implement authentication and authorization middleware
    feat(user): add login, profile, logout, and password update functionalities

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,9 +1,8 @@
-import bcrypt from "bcrypt";
+import bcrypt, { hash } from "bcrypt";
 import database from "../database/db.js";
 import {catchAsyncErrors} from "../middlewares/catchasync.middleware.js";
 import ErrorHandler from "../middlewares/error.middleware.js";
 import { sendToken } from "../utils/jwtToken.utils.js";
-
 
 
 const register=catchAsyncErrors(async(req,res,next)=>{
@@ -26,6 +25,69 @@ const register=catchAsyncErrors(async(req,res,next)=>{
     sendToken(user.rows[0],201,"Registered Successfully",res);
 });
 
+const login =catchAsyncErrors(async(req,res,next)=>{
+    const {email,password}=req.body;
+    if(!email||!password){
+        return next(new ErrorHandler("Please enter all fields",400));
+    }
+    const user=await database.query(`SELECT * FROM users WHERE email=$1`,[email]);
+    if(user.rows.length===0){
+        return next(new ErrorHandler("Invalid email or password",401));
+    }
+    const isPasswordMatch=await bcrypt.compare(password,user.rows[0].password);
+    if(!isPasswordMatch){
+        return next(new ErrorHandler("Invalid password",401));
+    }
+    sendToken(user.rows[0],200,"Logged in Successfully",res);
+})
+const logout =catchAsyncErrors(async(req,res,next)=>{
+    res.status(200).cookie("token","",{
+        expires:new Date(Date.now()),
+        httpOnly:true,
+    }).json({
+        success:true,
+        message:"Logged out successfully"
+    });
+});
+const profile =catchAsyncErrors(async(req,res,next)=>{
+    const {user}=req;
+    res.status(200).json({
+        success:true,
+        user
+    });
+});
+const updateProfile =catchAsyncErrors(async(req,res,next)=>{
+
+})
+const updatePassword =catchAsyncErrors(async(req,res,next)=>{
+    const{currentPassword,newPassword,confirmNewPassword}=req.body;
+    if(!currentPassword || !newPassword || !confirmNewPassword){
+        return next(new ErrorHandler("Please enter all fields",400));
+    }
+    const isPasswordMatch=await bcrypt.compare(currentPassword,req.user.password);
+    if(!isPasswordMatch){
+        return next(new ErrorHandler("Current password is incorrect",401));
+    }
+    if(newPassword!=confirmNewPassword){
+        return next(new ErrorHandler("New password and confirm new password do not match",400));
+    }
+    if(newPassword.length<8||newPassword.length>16||confirmNewPassword.length<8||confirmNewPassword.length>16){
+        return next(new ErrorHandler("Password must be between 8 and 16 characters",400));
+    }
+    const hashedPassword=await bcrypt.hash(newPassword,10);
+    await database.query(`UPDATE users SET password=$1 WHERE id=$2`,[hashedPassword,req.user.id]);
+    res.status(200).json({
+        success:true,
+        message:"Password updated successfully"
+    })
+})
+const forgotPassword =catchAsyncErrors(async(req,res,next)=>{
+
+})
+const resetPassword =catchAsyncErrors(async(req,res,next)=>{
+
+})
 
 
-export {register};
+
+export {register,login,logout,profile,updateProfile,updatePassword,forgotPassword,resetPassword};
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,11 +1,23 @@
 import express from "express";
 import {
   register,
+  login,
+  profile,
+  logout,
+  updateProfile,
+  forgotPassword,
+  resetPassword,
+  updatePassword
 } from "../controllers/user.controller.js";
-// import { isAuthenticated } from "../middlewares/authMiddleware.js";
-
+import { isAuthenticated } from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
 router.post("/register", register);
-
+router.post("/login", login);
+router.get("/me",isAuthenticated,profile);
+router.put("/me/update",isAuthenticated,updateProfile);
+router.put("/password/update",isAuthenticated,updatePassword);
+router.put("/password/reset/:token",resetPassword);
+router.post("/password/forgot",forgotPassword)
+router.get("/logout",isAuthenticated,logout);
 export default router;
