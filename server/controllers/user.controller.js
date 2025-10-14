1d825b00e8d9f11864550f18f5344a2202cfdcbd
saket <saket@saketgupta.com>
2025-10-14 19:16:10 +0530

    feat(auth): add password reset functionality with email notifications

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,93 +1,181 @@
 import bcrypt, { hash } from "bcrypt";
 import database from "../database/db.js";
-import {catchAsyncErrors} from "../middlewares/catchasync.middleware.js";
+import { catchAsyncErrors } from "../middlewares/catchasync.middleware.js";
 import ErrorHandler from "../middlewares/error.middleware.js";
 import { sendToken } from "../utils/jwtToken.utils.js";
+import { v2 as cloudinary } from "cloudinary";
+import crypto from "crypto";
+import { generateResetPasswordToken } from "../utils/generateResetPasswordToken.js";
+import { generateEmailTemplate } from "../utils/generateEmailTemplate.js";
+import { sendEmail } from "../utils/sendEmail.js";
 
 
-const register=catchAsyncErrors(async(req,res,next)=>{
-    const {name,email,password}=req.body;
-    if(!name || !email || !password){
-        return next(new ErrorHandler("Please enter all fields",400));
+const register = catchAsyncErrors(async (req, res, next) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+        return next(new ErrorHandler("Please enter all fields", 400));
     }
-    const isAlreadyRegistered=await database.query(
-        `SELECT * FROM users WHERE email=$1`,[email]
+    const isAlreadyRegistered = await database.query(
+        `SELECT * FROM users WHERE email=$1`, [email]
     );
-    if(isAlreadyRegistered.rows.length>0){
-        return next(new ErrorHandler("User already registered",400));
+    if (isAlreadyRegistered.rows.length > 0) {
+        return next(new ErrorHandler("User already registered", 400));
     }
-    const hashedPassword= await bcrypt.hash(password,10);
-    const user=await database.query(
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await database.query(
         `INSERT INTO users (name,email,password) VALUES ($1,$2,$3) RETURNING *`,
-        [name,email,hashedPassword]
+        [name, email, hashedPassword]
     );
     // there is another way of writing this query is using sql injection
-    sendToken(user.rows[0],201,"Registered Successfully",res);
+    sendToken(user.rows[0], 201, "Registered Successfully", res);
 });
 
-const login =catchAsyncErrors(async(req,res,next)=>{
-    const {email,password}=req.body;
-    if(!email||!password){
-        return next(new ErrorHandler("Please enter all fields",400));
+const login = catchAsyncErrors(async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return next(new ErrorHandler("Please enter all fields", 400));
     }
-    const user=await database.query(`SELECT * FROM users WHERE email=$1`,[email]);
-    if(user.rows.length===0){
-        return next(new ErrorHandler("Invalid email or password",401));
+    const user = await database.query(`SELECT * FROM users WHERE email=$1`, [email]);
+    if (user.rows.length === 0) {
+        return next(new ErrorHandler("Invalid email or password", 401));
     }
-    const isPasswordMatch=await bcrypt.compare(password,user.rows[0].password);
-    if(!isPasswordMatch){
-        return next(new ErrorHandler("Invalid password",401));
+    const isPasswordMatch = await bcrypt.compare(password, user.rows[0].password);
+    if (!isPasswordMatch) {
+        return next(new ErrorHandler("Invalid password", 401));
     }
-    sendToken(user.rows[0],200,"Logged in Successfully",res);
+    sendToken(user.rows[0], 200, "Logged in Successfully", res);
 })
-const logout =catchAsyncErrors(async(req,res,next)=>{
-    res.status(200).cookie("token","",{
-        expires:new Date(Date.now()),
-        httpOnly:true,
+const logout = catchAsyncErrors(async (req, res, next) => {
+    res.status(200).cookie("token", "", {
+        expires: new Date(Date.now()),
+        httpOnly: true,
     }).json({
-        success:true,
-        message:"Logged out successfully"
+        success: true,
+        message: "Logged out successfully"
     });
 });
-const profile =catchAsyncErrors(async(req,res,next)=>{
-    const {user}=req;
+const profile = catchAsyncErrors(async (req, res, next) => {
+    const { user } = req;
     res.status(200).json({
-        success:true,
+        success: true,
         user
     });
 });
-const updateProfile =catchAsyncErrors(async(req,res,next)=>{
-
-})
-const updatePassword =catchAsyncErrors(async(req,res,next)=>{
-    const{currentPassword,newPassword,confirmNewPassword}=req.body;
-    if(!currentPassword || !newPassword || !confirmNewPassword){
-        return next(new ErrorHandler("Please enter all fields",400));
+const updateProfile = catchAsyncErrors(async (req, res, next) => {
+    const { name, email } = req.body;
+    if (!name || !email) {
+        return next(new ErrorHandler("Please enter all fields", 400));
     }
-    const isPasswordMatch=await bcrypt.compare(currentPassword,req.user.password);
-    if(!isPasswordMatch){
-        return next(new ErrorHandler("Current password is incorrect",401));
+    if (name.trim().length === 0 || email.trim().length === 0) {
+        return next(new ErrorHandler("Fields cannot be empty", 400));
     }
-    if(newPassword!=confirmNewPassword){
-        return next(new ErrorHandler("New password and confirm new password do not match",400));
+    let avatarData = {};
+    if (req.files && req.files.avatar) {
+        const { avatar } = req.files;
+        // remove the old avatar from cloudinary
+        if (req.user?.avatar?.public_id) {
+            await cloudinary.uploader.destroy(req.user.avatar.public_id);
+        }
+        // if not present then add
+        const newProfileImage = await cloudinary.uploader.upload(
+            avatar.tempFilePath, {
+            folder: "Ecommerce_Avatars",
+            width: 150,
+            crop: "scale"
+        }
+        );
+        avatarData = {
+            public_id: newProfileImage.public_id,
+            url: newProfileImage.secure_url
+        }
+    };
+    let user;
+    if (Object.keys(avatarData).length === 0) {
+        user = await database.query(
+            "UPDATE users SET name=$1,email=$2 WHERE id=$3 RETURNING *", [name, email, req.user.id]
+        )
     }
-    if(newPassword.length<8||newPassword.length>16||confirmNewPassword.length<8||confirmNewPassword.length>16){
-        return next(new ErrorHandler("Password must be between 8 and 16 characters",400));
+    else {
+        user = await database.query(
+            "UPDATE users SET name=$1,email=$2,avatar=$3 WHERE id=$4 RETURNING *", [name, email, avatarData, req.user.id]
+        )
     }
-    const hashedPassword=await bcrypt.hash(newPassword,10);
-    await database.query(`UPDATE users SET password=$1 WHERE id=$2`,[hashedPassword,req.user.id]);
     res.status(200).json({
-        success:true,
-        message:"Password updated successfully"
+        success: true,
+        message: "Profile updated successfully",
+        user: user.rows[0]
+    });
+});
+const updatePassword = catchAsyncErrors(async (req, res, next) => {
+    const { currentPassword, newPassword, confirmNewPassword } = req.body;
+    if (!currentPassword || !newPassword || !confirmNewPassword) {
+        return next(new ErrorHandler("Please enter all fields", 400));
+    }
+    const isPasswordMatch = await bcrypt.compare(currentPassword, req.user.password);
+    if (!isPasswordMatch) {
+        return next(new ErrorHandler("Current password is incorrect", 401));
+    }
+    if (newPassword != confirmNewPassword) {
+        return next(new ErrorHandler("New password and confirm new password do not match", 400));
+    }
+    if (newPassword.length < 8 || newPassword.length > 16 || confirmNewPassword.length < 8 || confirmNewPassword.length > 16) {
+        return next(new ErrorHandler("Password must be between 8 and 16 characters", 400));
+    }
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    await database.query(`UPDATE users SET password=$1 WHERE id=$2`, [hashedPassword, req.user.id]);
+    res.status(200).json({
+        success: true,
+        message: "Password updated successfully"
     })
 })
-const forgotPassword =catchAsyncErrors(async(req,res,next)=>{
+const forgotPassword = catchAsyncErrors(async (req, res, next) => {
+    const { email } = req.body;
+    const {frontendUrl}=req.query;
+    if (!email) {
+        return next(new ErrorHandler("Please enter your email", 400));
+    }
+    let userResult=await database.query(
+        `SELECT * FROM users WHERE email=$1`,[email]
+    )
+    if(userResult.rows.length===0){
+        return next(new ErrorHandler("No user found",400));
+    }
+    const user=userResult.rows[0];
+    const {hashedToken,resetPasswordExpireTime,resetToken}=generateResetPasswordToken();
+    await database.query(
+        `UPDATE users SET reset_password_token = $1, reset_password_expire = to_timestamp($2) WHERE email = $3`,
+    [hashedToken, resetPasswordExpireTime / 1000, email]
+    );
+    const resetPasswordUrl=`${frontendUrl}/password/reset/${resetToken}`;
+    const message=generateEmailTemplate(resetPasswordUrl);
+
+    try {
+        await sendEmail({
+            email:user.email,
+            subject:"Ecommerce Password Recovery",
+            message,
+        });
+        res.status(200).json({
+            success:true,
+            message:`Email send to ${user.email} successfully`
+        });
+    } catch (error) {
+        await database.query(
+            `UPDATE users SET reset_password_token=NULL,reset_password_expire=NULL WHERE email=$1`,[email]
+        );
+        return next (new ErrorHandler("Email could not be send",400));
+    }
 
 })
-const resetPassword =catchAsyncErrors(async(req,res,next)=>{
+const resetPassword = catchAsyncErrors(async (req, res, next) => {
+    // const{token}=req.params;
+    // const resetPasswordToken=crypto.createHash('sha256').update(token).digest("hex");
+    // const user=await database.query(
+    //     `SELECT * FROM users WHERE reset_password_token=$1 AND reset_password_expire>NOW()`,[resetPasswordToken]
+    // );
 
 })
 
 
 
-export {register,login,logout,profile,updateProfile,updatePassword,forgotPassword,resetPassword};
+export { register, login, logout, profile, updateProfile, updatePassword, forgotPassword, resetPassword };
