f4ccff345eb1d85334dd7e2dee274ed45a8bc000
saket <saket@saketgupta.com>
2025-10-22 23:30:23 +0530

    feat(product): enhance product management with CRUD operations and filters

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -10,8 +10,8 @@ const createProduct = catchAsyncErrors(async (req, res, next) => {
     if (!name || !description || !price || !stock || !category) {
         return next(new ErrorHandler("Please provide complete product details", 400))
     }
-    const USDToINRRate=await getUSDToINRRate();
-    const priceInUSD=price/USDToINRRate;
+    const USDToINRRate = await getUSDToINRRate();
+    const priceInUSD = price / USDToINRRate;
 
     let uploadedImages = [];
     if (req.files && req.files.images) {
@@ -34,71 +34,225 @@ const createProduct = catchAsyncErrors(async (req, res, next) => {
     }
 
     const product = await database.query(
-    `INSERT INTO products (name, description, price, category, stock, images, created_by) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
-    [
-      name,
-      description,
-      priceInUSD,
-      category,
-      stock,
-      JSON.stringify(uploadedImages),
-      created_by,
-    ]
-  )
-  res.status(201).json({
-    success:true,
-    message:"Product created successfully",
-    product:product.rows[0]
-  })
+        `INSERT INTO products (name, description, price, category, stock, images, created_by) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
+        [
+            name,
+            description,
+            priceInUSD,
+            category,
+            stock,
+            JSON.stringify(uploadedImages),
+            created_by,
+        ]
+    )
+    res.status(201).json({
+        success: true,
+        message: "Product created successfully",
+        product: product.rows[0]
+    })
 
 
 });
-const productDetails=catchAsyncErrors(async(req,res,next)=>{
-})
-const fetchAllProduct=catchAsyncErrors(async(req,res,next)=>{
-    const {availability,price,category,ratings,search}=req.query;
-    const page=parseInt(req.query.page)||1;
-    const limit=10;
-    const offset=(page-1)*limit;
-
-    const conditions=[];
-    let values=[];
-    let index=1;
-
-    let paginationPlaceholders={};
-
-    if(availability==="in-stock"){
-        conditions.push(`stock >5`);
+const productDetails = catchAsyncErrors(async (req, res, next) => {
+    const { productId } = req.body;
+    const product = await database.query("SELECT * FROM products WHERE id=$1", [productId]);
+    if (product.rows.length === 0) {
+        return next(new ErrorHandler("Product not found", 404));
     }
-    else if(availability==="limited"){
-        conditions.push(`stock >5`);
-    }
-    else if(availability==="out-of-stock"){
-        conditions.push(`stock=0`);
+    const result=await database.query(
+        `
+            SELECT p.*,
+        COALESCE(
+        json_agg(
+        json_build_object(
+            'review_id', r.id,
+            'rating', r.rating,
+            'comment', r.comment,
+            'reviewer', json_build_object(
+            'id', u.id,
+            'name', u.name,
+            'avatar', u.avatar
+            )) 
+        ) FILTER (WHERE r.id IS NOT NULL), '[]') AS reviews
+         FROM products p
+         LEFT JOIN reviews r ON p.id = r.product_id
+         LEFT JOIN users u ON r.user_id = u.id
+         WHERE p.id  = $1
+         GROUP BY p.id
+        `,[productId]
+    );
+    res.status(200).json({
+        success:true,
+        message:"Product fetched successfully",
+        product:result.rows[0],
+    });
+});
+// in this we will be modifying the controller according the query params as we will be having the filters on the frontend
+const fetchAllProducts = catchAsyncErrors(async (req, res, next) => {
+    const { availability, price, category, ratings, search } = req.query;
+    const page = parseInt(req.query.page) || 1;
+    const limit = 10;
+    const offset = (page - 1) * limit;
+
+    const conditions = [];
+    let values = [];
+    let index = 1;
+    let paginationPlaceholders = {};
+
+    // availability filter
+    if (availability === "in-stock") {
+        conditions.push(`stock > 5`);
+    } else if (availability === "limited") {
+        conditions.push(`stock > 0 AND stock <= 5`);
+    } else if (availability === "out-of-stock") {
+        conditions.push(`stock = 0`);
     }
 
-    if(price){
-        const [minPrice,maxPrice]=price.split("-");
-        if(minPrice &&maxPrice){
-            conditions.push(`price BETWEEN $${index} AND $${index+1}`);
-            values.push(minPrice,maxPrice);
-            index+=2;
+    // price filter
+    if (price) {
+        const [minPrice, maxPrice] = price.split("-");
+        const min = parseFloat(minPrice)
+        const max = parseFloat(maxPrice)
+        if (!isNaN(min) && !isNaN(max)) {
+            conditions.push(`price BETWEEN $${index} AND $${index + 1}`);
+            values.push(minPrice, maxPrice);
+            index += 2;
         }
     }
-    if(category){
-        conditions.push(`category ILIKE$${index}`);
+
+    // category filter
+    if (category) {
+        conditions.push(`category ILIKE $${index}`);
         values.push(`%${category}%`);
         index++;
     }
-    if(ratings){
-        conditions.push(`ratings >=$${index}`);
+
+    // ratings filter
+    if (ratings) {
+        conditions.push(`ratings >= $${index}`);
         values.push(ratings);
         index++;
     }
-    if(search){
+
+    // search filter
+    if (search) {
         conditions.push(`(p.name ILIKE $${index} OR p.description ILIKE $${index})`);
         values.push(`%${search}%`);
         index++;
     }
+
+    const whereClause = conditions.length ? `WHERE ${conditions.join(" AND ")}` : "";
+
+    // total count
+    const totalProductsResult = await database.query(
+        `SELECT COUNT(*) FROM products p ${whereClause}`,
+        values
+    );
+
+    const totalProducts = parseInt(totalProductsResult.rows[0].count);
+
+    paginationPlaceholders.limit = `$${index}`;
+    values.push(limit);
+    index++;
+
+    paginationPlaceholders.offset = `$${index}`;
+    values.push(offset);
+    index++;
+
+    const query = `
+    SELECT p.*, COUNT(r.id) AS review_count
+    FROM products p
+    LEFT JOIN reviews r ON p.id = r.product_id
+    ${whereClause}
+    GROUP BY p.id
+    ORDER BY p.created_at DESC
+    LIMIT ${paginationPlaceholders.limit}
+    OFFSET ${paginationPlaceholders.offset};
+  `;
+
+    const result = await database.query(query, values);
+
+    // new products
+    const newProductsQuery = `
+    SELECT p.*, COUNT(r.id) AS review_count
+    FROM products p
+    LEFT JOIN reviews r ON p.id = r.product_id
+    WHERE p.created_at >= NOW() - INTERVAL '30 days'
+    GROUP BY p.id
+    ORDER BY p.created_at DESC
+    LIMIT 8;
+  `;
+    const newProductsResult = await database.query(newProductsQuery);
+
+    // top rated
+    const topRatedQuery = `
+    SELECT p.*, COUNT(r.id) AS review_count
+    FROM products p
+    LEFT JOIN reviews r ON p.id = r.product_id
+    WHERE p.ratings >= 4.5
+    GROUP BY p.id
+    ORDER BY p.ratings DESC, p.created_at DESC
+    LIMIT 8;
+  `;
+    const topRatedResult = await database.query(topRatedQuery);
+
+    res.status(200).json({
+        success: true,
+        products: result.rows,
+        totalProducts,
+        newProducts: newProductsResult.rows,
+        topRatedProducts: topRatedResult.rows,
+    });
+});
+
+
+const updateProduct = catchAsyncErrors(async (req, res, next) => {
+    const { productId } = req.params;
+    const { name, description, price, category, stock } = req.body;
+    if (!name || !description || !price || !category || !stock) {
+        return next(
+            new ErrorHandler("Please provide complete product details")
+        );
+    }
+    const product = await database.query("SELECT * FROM products WHERE id=$1", [productId]);
+    if (product.rows.length === 0) {
+        return next(new ErrorHandler("Product not found", 404));
+    }
+    const USDToINRRate = await getUSDToINRRate();
+    const priceInUSD = price / USDToINRRate;
+    const result = await database.query(
+        `UPDATE products SET name=$1,description=$2,price=$3,category=$4,stock=$5 WHERE id=$6 RETURNING *`, [name, description, priceInUSD, category, stock, productId]
+    );
+    res.status(200).json({
+        success: true,
+        message: "Product updated successfully",
+        updatedProduct: result.rows[0],
+    });
 })
-export {createProduct,productDetails,fetchAllProduct};
+
+const deleteProduct = catchAsyncErrors(async (req, res, next) => {
+    const { productId } = req.params;
+    const product = await database.query("SELECT * FROM products WHERE id=$1", [productId]);
+    if (product.rows.length === 0) {
+        return next(new ErrorHandler("Product not found", 404));
+    }
+    const images = product[0].images;
+    const deleteResult = await database.query(
+        "DELETE FROM products WHERE id=$1 RETURNING *", [productId]
+    );
+    if (deleteResult.rows.length === 0) {
+        return next(new ErrorHandler("Failed to delete the product", 500));
+    }
+    if (images && images.length > 0) {
+        for (const image of images) {
+            await cloudinary.uploader.destroy(image.public_id);
+        }
+    }
+    res.status(200).json({
+        success: true,
+        message: "Product deleted successfully"
+    })
+
+})
+
+export { createProduct, productDetails, fetchAllProducts, updateProduct, deleteProduct };
--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { createProduct,productDetails } from "../controllers/product.controller.js";
+import { createProduct,deleteProduct,fetchAllProducts,productDetails, updateProduct } from "../controllers/product.controller.js";
 import { isAuthenticated,authorizedRoles } from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
 router.post("/create",isAuthenticated,authorizedRoles("Admin"),createProduct);
+router.post("/admin/update/:productId",isAuthenticated,authorizedRoles("Admin"),updateProduct);
+router.delete("/delete/:productId",isAuthenticated,authorizedRoles("Admin"),deleteProduct);
+router.get("/",fetchAllProducts)
+router.get("/:productId",productDetails)
 
 export default router;
