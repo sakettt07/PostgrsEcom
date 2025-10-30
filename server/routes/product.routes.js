08ad619c7a97eab24582702caecedc2eade1eba4
saket <saket@saketgupta.com>
2025-10-30 23:59:14 +0530

    fix(routes): correct syntax by adding missing semicolons in product routes

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -6,8 +6,8 @@ const router = express.Router();
 router.post("/create",isAuthenticated,authorizedRoles("Admin"),createProduct);
 router.post("/admin/update/:productId",isAuthenticated,authorizedRoles("Admin"),updateProduct);
 router.delete("/delete/:productId",isAuthenticated,authorizedRoles("Admin"),deleteProduct);
-router.get("/",fetchAllProducts)
-router.get("/:productId",productDetails)
+router.get("/",fetchAllProducts);
+router.get("/:productId",productDetails);
 router.post("/add/review/:productId",isAuthenticated,addReview);
 router.delete("/delete/review/:productId", isAuthenticated, deleteReview);
 
