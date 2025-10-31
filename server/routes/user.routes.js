1d33da101f3cc53a841b29df3583455e05f17095
saket <saket@saketgupta.com>
2025-10-31 23:56:46 +0530

    fix(routes): add missing comment for admin-only routes in user routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -22,6 +22,7 @@ router.put("/password/update",isAuthenticated,updatePassword);
 router.put("/password/reset/:token",resetPassword);
 router.post("/password/forgot",forgotPassword)
 router.get("/logout",isAuthenticated,logout);
+// the below routes can only be accessed by the admin
 router.delete("/delete/user",isAuthenticated,authorizedRoles("Admin"),deleteUser);
 router.get("/users/all",isAuthenticated,authorizedRoles("Admin"),getAllUsers)
 export default router;
