b56bc55720a613e50f9159906b2bce3577b21f18
saket <saket@saketgupta.com>
2025-11-12 23:59:38 +0530

    feat(ai): implement AI recommendation function for product filtering

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createProduct,deleteProduct,fetchAllProducts,productDetails, updateProduct,deleteReview,addReview } from "../controllers/product.controller.js";
+import { createProduct,deleteProduct,fetchAllProducts,productDetails, updateProduct,deleteReview,addReview, fetchAIFilteredProducts } from "../controllers/product.controller.js";
 import { isAuthenticated,authorizedRoles } from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
 router.post("/create",isAuthenticated,authorizedRoles("Admin"),createProduct);
 router.delete("/delete/:productId",isAuthenticated,authorizedRoles("Admin"),deleteProduct);
 router.get("/",fetchAllProducts);
+router.post("/ai-search",isAuthenticated,fetchAIFilteredProducts)
 router.get("/:productId",productDetails);
 router.post("/add/review/:productId",isAuthenticated,addReview);
 router.post("/admin/update/:productId",isAuthenticated,authorizedRoles("Admin"),updateProduct);
